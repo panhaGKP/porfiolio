b6ba1995eb8d54a665466fe00d6919c9fd31f641
Panha <panhajack012@gmail.com>
2022-08-25 14:35:34 +0700

    Finish the Testimonial part with brief detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
@@ -26,3 +27,70 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n=>n.addEventListener('click', linkAction))
+
+/*========Show Accordingly skill */
+const skillsContent = document.getElementsByClassName('skills__content')
+const skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass  = this.parentNode.className
+    for(i=0 ; i< skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass == 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click', toggleSkills)
+})
+
+/*===========Qualification Tab============== */
+const tabs = document.querySelectorAll('[data-target')
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab=>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent=>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+        tabs.forEach(tab=>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+/*================SERVICE MODAL====================*/ 
+const modalViews = document.querySelectorAll('.services__modal')
+const modalBtns = document.querySelectorAll('.services__button')
+const modalCloses = document.querySelectorAll('.services__modal-close')
+
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach(
+    (modalBtn, i)=> {
+        modalBtn.addEventListener('click', ()=>{
+            modal(i)
+        })
+    }
+)
+
+modalCloses.forEach(
+    (modalClose)=>{
+        modalClose.addEventListener('click', ()=>{
+            modalViews.forEach(
+                (modalView)=>{
+                    modalView.classList.remove('active-modal')
+                }
+            )
+        })
+    }
+)
+
+// ============PORTFIOLIO SWIPER==================== No use
